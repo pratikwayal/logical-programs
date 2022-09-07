92e4f418d0a0b10868a50da62f1254ec6e9e98c2
DjS <DjS@PRATIKSPC>
2022-09-07 22:16:49 +0530

    find l num fs new

---
+++ logical_program/src/logical_program/Find_largest_number_3_num.java
@@ -0,0 +1,27 @@
+package logical_program;
+
+import java.util.Scanner;
+
+public class Find_largest_number_3_num {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int a= sc.nextInt();
+		System.out.println("Enter 2nd number");
+int b=sc.nextInt();
+System.out.println("Eneter 3rd number");
+int c = sc.nextInt();
+ if (a>b && a>c) {
+	 System.out.println(a + " is largest number");
+ }
+	if (b>a && b>c)	 {
+		 System.out.println(b+ "  is largest number");
+
+	}
+	else
+		 System.out.println(c+  " is largest number");
+
+	}
+
+}
---
+++ logical_program/src/numericpattren/FSNEW.java
@@ -0,0 +1,18 @@
+package numericpattren;
+
+public class FSNEW {
+ public static void main(String[] args) {
+	int n=0;
+	int n1=1;
+	int n2=6;    // number of count
+	int sum;
+	
+	for (int i=1;i<=n2;i++) {
+		sum=n+n1;
+		System.out.println(n);
+		n=n1;
+		n1=sum;
+		
+	}
+}
+}
