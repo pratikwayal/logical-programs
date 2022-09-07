7c9edc29b9bbbd0214c78be3c2bbd888ca7bc7ba
DjS <DjS@PRATIKSPC>
2022-09-07 22:11:20 +0530

    armstrong

---
+++ logical_program/src/logical_program/Armstrongnumber_1.java
@@ -0,0 +1,27 @@
+package logical_program;
+
+public class Armstrongnumber_1 {
+
+	public static void main(String[] args) {
+		
+
+		int n=371 ;
+		int number=n;
+		int r,sum=0;
+		while (n>0)
+		{
+			r=n%10;
+			n=n/10;
+			sum=sum + r*r*r;
+			
+		}
+		if (number==sum)
+			System.out.println("Its an armsstrong number");
+		else
+			System.out.println("Its an not armsstrong number");
+	}
+
+}
+
+
+
