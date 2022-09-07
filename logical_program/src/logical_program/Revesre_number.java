4defe98c7525ec3261c07a1911e5b211eb036b78
DjS <DjS@PRATIKSPC>
2022-09-07 22:14:50 +0530

    r number

---
+++ logical_program/src/logical_program/Revesre_number.java
@@ -0,0 +1,29 @@
+package logical_program;
+
+import java.util.Scanner;
+
+public class Revesre_number {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Enter a number");
+	int num=sc.nextInt();
+	int org_num= num;  // for palindrome
+	//12345
+	//1 using algorithm
+	int rev=0;
+	while(num!=0) {
+		rev=rev*10 +num%10;
+		num=num/10;	
+	}
+System.out.println("Reverse number is: " +rev);
+ // for the palindrome number check
+if (org_num==rev) {
+	System.out.println(org_num +" is palindrome number");
+}
+else {
+	System.out.println(org_num +" is not palindrome number");
+}
+	}
+
+}
---
+++ logical_program/src/numericpattren/arraylength.java
@@ -0,0 +1,17 @@
+package numericpattren;
+
+public class arraylength {
+public static void main(String[] args) {
+	
+	int [] k = new int [12];
+	int arraylength =k.length;
+	System.out.println("The length of array is"+arraylength);
+}
+	
+	
+	
+	
+
+}
+
+
