c2f8f9ea88e227762d6b5d9175cfb86fad5a7b1a
DjS <DjS@PRATIKSPC>
2022-09-07 22:17:41 +0530

    multiple table number digit in number

---
+++ logical_program/src/logical_program/multiplication_table.java
@@ -0,0 +1,21 @@
+package logical_program;
+
+import java.util.Scanner;
+
+public class multiplication_table {
+
+//	public static void main(String[] args) {
+//		int num=7;
+//		for ( int i=1;i<=10;i++) {
+//		
+//			System.out.println(num*i);
+//		}
+//		
+//		
+//
+//	}
+	public static void main(String[] args) {
+			System.out.println("");
+	//my name is \'pratik\".
+
+}}
---
+++ logical_program/src/logical_program/number_digits_in_number.java
@@ -0,0 +1,16 @@
+package logical_program;
+
+public class number_digits_in_number {
+
+	public static void main(String[] args) {
+		int num=123456;
+		int count =0;
+		while(num>0) {
+			num=num/10;
+			count++;
+			
+		}
+System.out.println(count);
+	}
+
+}
